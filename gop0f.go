10d089ffcda803d415bdc0791f67ed89d3c55e0c
kali kaneko <kali@leap.se>
2021-11-13 22:13:45 +0100

    parse response

--- gop0f.go
+++ gop0f.go
@@ -1,12 +1,12 @@
 package gop0f
 
 import (
-	//	"bytes"
-	//	"encoding/binary"
-	//	"encoding/hex"
 	"bytes"
+	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net"
 )
 
@@ -31,7 +31,7 @@ type GoP0f struct {
 	socket string
 }
 
-type P0fResponse struct {
+type p0fResponse struct {
 	Magic      uint32                // Must be P0F_RESP_MAGIC
 	Status     uint32                // P0F_STATUS_*
 	FirstSeen  uint32                // First seen (unix time)
@@ -41,7 +41,7 @@ type P0fResponse struct {
 	UpModDays  uint32                // Uptime modulo (days)
 	LastNat    uint32                // NAT / LB last detected (unix time)
 	LastChg    uint32                // OS chg last detected (unix time)
-	Distance   uint32                // System distance
+	Distance   uint16                // System distance
 	BadSw      byte                  // Host is lying about U-A / Server
 	OsMatchQ   byte                  // Match quality
 	OsName     [P0F_STR_MAX + 1]byte // Name of detected OS
@@ -50,6 +50,54 @@ type P0fResponse struct {
 	HttpFlavor [P0F_STR_MAX + 1]byte // Flavor of detected HTTP app
 	LinkType   [P0F_STR_MAX + 1]byte // Link type
 	Language   [P0F_STR_MAX + 1]byte // Language
+}
+
+type OSMatchQuality int
+
+const (
+	Normal OSMatchQuality = 0 + iota
+	Fuzzy
+	Signature
+	FuzzySignature
+)
+
+type IPInfo struct {
+	OsMatchQ   OSMatchQuality // Match quality
+	BadSw      int            // Host is lying about U-A / Server
+	Distance   uint16         // System distance
+	OsName     string         // Name of detected OS
+	OsFlavor   string         // Flavor of detected OS
+	HttpName   string         // Name of detected HTTP app
+	HttpFlavor string         // Flavor of detected HTTP app
+	LinkType   string         // Link type
+	Language   string         // Language
+}
+
+func NewInfo(r p0fResponse) (info IPInfo, err error) {
+	// TODO check magic
+	if r.Status != P0F_STATUS_OK {
+		e := "UNKNOWNERR"
+		switch r.Status {
+		case P0F_STATUS_NOMATCH:
+			e = "NOMATCH"
+		case P0F_STATUS_BADQUERY:
+			e = "BADQUERY"
+		}
+		return IPInfo{}, errors.New(e)
+
+	}
+	i := IPInfo{
+		OsMatchQ:   OSMatchQuality(r.OsMatchQ),
+		BadSw:      int(r.BadSw),
+		Distance:   r.Distance,
+		OsName:     toString(r.OsName),
+		OsFlavor:   toString(r.OsFlavor),
+		HttpName:   toString(r.HttpName),
+		HttpFlavor: toString(r.HttpFlavor),
+		LinkType:   toString(r.LinkType),
+		Language:   toString(r.Language),
+	}
+	return i, nil
 }
 
 func New(sock string) (p0f *GoP0f, err error) {
@@ -67,27 +115,23 @@ func (p0f *GoP0f) Close() {
 	p0f.conn.Close()
 }
 
-func (p0f *GoP0f) Query(addr net.IP) (resp P0fResponse, err error) {
+func (p0f *GoP0f) Query(addr net.IP) (resp p0fResponse, err error) {
 	// Query = 21 bytes
 	// Magic + 4 (ipv4) + ipaddr bytes + padding
 	ip := []byte(addr)[len(addr)-4 : len(addr)]
 	query := P0F_QUERY_MAGIC
-	query = append(query[:], []byte{4}...)
+	query = append(query[:], []byte{P0F_ADDR_IPV4}...)
 	query = append(query[:], ip...)
 	query = append(query[:], bytes.Repeat([]byte{0}, 12)...)
-	fmt.Println(query)
 
 	_, err = p0f.conn.Write(query)
 	if err != nil {
 		return
 	}
 
-	r := P0fResponse{}
-
+	r := p0fResponse{}
 	data := make([]byte, 1024)
-	zeroes := 0
 	n, err := p0f.conn.Read(data[:])
-	fmt.Printf("Got %d bytes\n", n)
 	if err != nil {
 		if err != io.EOF {
 			fmt.Println(err)
@@ -95,13 +139,23 @@ func (p0f *GoP0f) Query(addr net.IP) (resp P0fResponse, err error) {
 		}
 	}
 	data = data[:n]
-	fmt.Println(data)
-	for _, b := range data {
-		if b != 0 {
-			//fmt.Printf("%v\n", b)
-		} else {
-			zeroes++
-		}
+	buf := bytes.NewReader(data)
+	err = binary.Read(buf, binary.LittleEndian, &r)
+	if err != nil {
+		log.Fatalf("binary.Read failed: %v", err)
 	}
+
+	//fmt.Printf("out = %#v\n", r)
+
+	info, err := NewInfo(r)
+	if err != nil {
+		fmt.Println("Error: " + err.Error())
+	}
+	fmt.Println(info)
 	return r, nil
+}
+
+func toString(ba [32]byte) string {
+	n := bytes.IndexByte(ba[:], 0)
+	return string(ba[:n])
 }
